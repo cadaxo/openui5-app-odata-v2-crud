4809f8f651942bf68bf6432186f08a3e37546d84
Dusan Sacha <dusan.sacha@cadaxo.com>
2017-02-09 10:39:28 +0000

    Mockserver added

---
+++ webapp/localService/mockserver.js
@@ -0,0 +1,24 @@
+sap.ui.define([
+	"sap/ui/core/util/MockServer"
+], function (MockServer) {
+	"use strict";
+	return {
+		init: function () {
+			// create
+			var oMockServer = new MockServer({
+				rootUri: "/destinations/northwind2/"
+			}); 
+			var oUriParameters = jQuery.sap.getUriParameters();
+			// configure
+			MockServer.config({
+				autoRespond: true,
+				autoRespondAfter: oUriParameters.get("serverDelay") || 1000
+			});
+			// simulate
+			var sPath = jQuery.sap.getModulePath("cadaxo.ui5.app.localService");
+			oMockServer.simulate(sPath + "/metadata.xml", sPath + "/mockdata");
+			// start
+			oMockServer.start();
+		}
+	};
+});
